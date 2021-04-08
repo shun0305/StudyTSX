4bd52d471b38f486564c031031ada7f13b3925b9
shun0305 <shunbbra@icloud.com>
2021-04-08 18:59:49 +0900

    ss

--- src/screens/HomeScreen.tsx
+++ src/screens/HomeScreen.tsx
@@ -11,7 +11,7 @@ const HomeScreen = () => {
         <Text> Home Screen </Text>
       </View>
       <View style={styles.footer}>
-        <Text> kokoko </Text>
+        <Text> s </Text>
       </View>
     </View>
   );
